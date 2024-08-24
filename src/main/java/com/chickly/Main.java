b16355fd05520b0fa04031317450ad32c943b263
Mahmoud Essam <144367882+RoadmannCoder@users.noreply.github.com>
2024-08-25 01:32:08 +0300

    Update Main.java

--- src/main/java/com/chickly/Main.java
+++ src/main/java/com/chickly/Main.java
@@ -2,6 +2,6 @@ package com.chickly;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello gays!");
+        System.out.println("Hello World!");
     }
 }
