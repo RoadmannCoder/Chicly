ffd1e37b62b0542d270ddadf534512d65c34b730
ahmedmandour <ahmedmandor@rocketmail.com>
2024-08-25 23:45:26 +0300

    Customer Account Category Done

---
+++ src/main/java/com/chickly/DataAccessLayer/Entities/Address.java
@@ -0,0 +1,24 @@
+package com.chickly.DataAccessLayer.Entities;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.*;
+
+
+@Embeddable
+@Getter
+@Setter
+@AllArgsConstructor
+@RequiredArgsConstructor
+@NoArgsConstructor
+public class Address {
+    @NonNull
+    private String street;
+    @NonNull
+    private String city;
+    @NonNull
+    private String state;
+    @NonNull
+    private String zip;
+    private String description;
+}
--- src/main/java/com/chickly/DataAccessLayer/Entities/Customer.java
+++ src/main/java/com/chickly/DataAccessLayer/Entities/Customer.java
@@ -1,21 +1,47 @@
 package com.chickly.DataAccessLayer.Entities;
 
 import jakarta.persistence.*;
-import lombok.Getter;
-import lombok.Setter;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import lombok.*;
+
+
+import java.math.BigDecimal;
+import java.util.Date;
 
 @Entity
 @Table(name = "customer")
+@RequiredArgsConstructor
+@NoArgsConstructor
 public class Customer {
     @Getter
     @Setter
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    private Integer id;
 
+    @NonNull
+    @Size(min = 2,max = 50 , message = "must be between 2 to 50 characters")
     private String firstName;
+    @NonNull
+    @Size(min = 2,max = 50 , message = "must be between 2 to 50 characters")
     private String lastName;
+    @NonNull
+    private BigDecimal creditLimit;
+    @NonNull
+    @Temporal(TemporalType.DATE)
+    private Date dateOfBirth;
+    @NonNull
+    @Pattern(regexp = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$", message = "Invalid email address")
+    private String email;
+    @NonNull
+    private String phoneNumber;
 
+    @NonNull
+    @Embedded
+    private Address address;
+    @NonNull
     @Embedded
     private Account account;
 }
