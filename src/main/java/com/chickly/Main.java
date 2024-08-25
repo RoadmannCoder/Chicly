8d8d28d774d398c282e8b74043b95fa134d62731
RoadmannCoder <mmensheteh@gmail.com>
2024-08-25 22:43:02 +0300

    Made CustomerRepository extends GenericCrudManager

--- src/main/java/com/chickly/Main.java
+++ src/main/java/com/chickly/Main.java
@@ -1,14 +1,11 @@
 package com.chickly;
 
-import com.chickly.DataAccessLayer.Entities.Admin;
-import jakarta.persistence.EntityManager;
-import jakarta.persistence.EntityManagerFactory;
-import jakarta.persistence.Persistence;
+
+
 
 public class Main {
 
     public static void main(String[] args) {
-        EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("chicly");
-        EntityManager entityManager = entityManagerFactory.createEntityManager();
+
     }
 }
