95fb3ce26bf74b0867a6a29f0d20bb49fd8a5811
ahmedmandour <ahmedmandor@rocketmail.com>
2024-08-26 23:54:27 +0300

    Adding Order Entity

--- src/main/java/com/chickly/DataAccessLayer/Entities/Order.java
+++ src/main/java/com/chickly/DataAccessLayer/Entities/Order.java
@@ -1,14 +1,31 @@
 package com.chickly.DataAccessLayer.Entities;
 
 import jakarta.persistence.*;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
 
-//@Entity
+import java.util.Date;
+
+@Entity
 @Table(name = "order")
 @Getter
 @Setter
-
+@NoArgsConstructor
+@RequiredArgsConstructor
 public class Order{
+    @Id
+    private Integer id;
+    @NonNull
+    private Status status = Status.PENDING;
 
+    @CreationTimestamp
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(updatable = false)
+    private Date createdAt;
+
+    public enum Status {
+        PENDING,
+        WAITING,
+        COMPLETED
+    }
 }
