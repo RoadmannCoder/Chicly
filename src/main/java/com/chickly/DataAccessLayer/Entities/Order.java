1918d8312ba0bdc10cd34c1b33c6bcd5861ff7bc
Mandour <ahmedmandour@gmail.com>
2024-08-26 23:53:06 +0300

    Adding Order Entity

---
+++ src/main/java/com/chickly/DataAccessLayer/Entities/Order.java
@@ -0,0 +1,14 @@
+package com.chickly.DataAccessLayer.Entities;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+//@Entity
+@Table(name = "order")
+@Getter
+@Setter
+
+public class Order{
+
+}
