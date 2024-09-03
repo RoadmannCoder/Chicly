7d867cd252f4d1e98e59fe43325d2706c4b1c015
ahmedmandour <ahmedmandor@rocketmail.com>
2024-09-03 10:51:51 +0300

    refactored entities validators and unit tests

--- src/main/java/com/chickly/DataAccessLayer/Entities/Account.java
+++ src/main/java/com/chickly/DataAccessLayer/Entities/Account.java
@@ -3,6 +3,8 @@ package com.chickly.DataAccessLayer.Entities;
 import jakarta.persistence.Column;
 import jakarta.persistence.Embeddable;
 
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import lombok.*;
 
 
@@ -13,8 +15,10 @@ import lombok.*;
 @NoArgsConstructor
 public class Account {
     @NonNull
+    @NotEmpty
     @Column(unique = true)
     private String userName;
     @NonNull
+    @NotEmpty
     private String password;
 }
--- src/main/java/com/chickly/DataAccessLayer/Entities/Address.java
+++ src/main/java/com/chickly/DataAccessLayer/Entities/Address.java
@@ -1,6 +1,7 @@
 package com.chickly.DataAccessLayer.Entities;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 import lombok.*;
 
@@ -13,11 +14,14 @@ import lombok.*;
 @NoArgsConstructor
 public class Address {
     @NonNull
+    @NotEmpty
     private String street;
     @NonNull
+    @NotEmpty
     private String city;
 
     @NonNull
+    @NotEmpty
     private String zip;
     private String description;
 }
